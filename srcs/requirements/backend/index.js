3d96580cb22383a9fbd92e4f447a1792e3269c13
Jeonghwan Hwang <jhwang@c2r10s1.42seoul.kr>
2024-08-01 20:37:08 +0900

    change var name && add rabbitmq connect

--- srcs/requirements/backend/index.js
+++ srcs/requirements/backend/index.js
@@ -2,33 +2,58 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mysql = require('mysql2/promise');
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 require('dotenv').config();
 
 const port = 5000;
-const RABBITMQ_URL = 'amqp://rabbitmq';
 
-let connection;
-
-const connectWithRetry = async () => {
+let mysqlConn;
+const mysqlConntWithRetry = async () => {
   const retryInterval = 3000; // 재시도 간격 (밀리초 단위)
-  while (!connection) {
+  while (!mysqlConn) {
     try {
       await new Promise(resolve => setTimeout(resolve, retryInterval));
-      connection = await mysql.createConnection({
+      mysqlConn = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
       });
-      console.log('Connected to database.');
     } catch (error) {
       console.error('Database connection failed:', error.stack);
       console.log(`Retrying database connection in ${retryInterval / 1000} seconds...`);
     }
   }
+  console.log('Connected to database.');
 };
-connectWithRetry();
+mysqlConntWithRetry();
+
+let rabbitMQConn;
+const RABBITMQ_URL = `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}`;
+
+const rabbitMQConntWithRetry = async () => {
+  const retryInterval = 3000; // 재시도 간격 (밀리초 단위)
+  while (!rabbitMQConn) {
+    try {
+      await new Promise(resolve => setTimeout(resolve, retryInterval));
+      rabbitMQConn = await amqp.connect(RABBITMQ_URL);
+      console.log('connect success: ' + rabbitMQConn);
+
+      process.once('SIGINT', async () => {
+        if (rabbitMQConn) {
+          await rabbitMQConn.close();
+          console.log('RabbitMQ connection closed.');
+        }
+        process.exit(0); // 정상 종료를 위해 exit 호출
+      });
+    } catch (error) {
+      console.error('Connection error:', error);
+      console.log(`Retrying Message Queue connection in ${retryInterval / 1000} seconds...`);
+    }
+  }
+  console.log('Connected to Message Queue.');
+};
+rabbitMQConntWithRetry();
 
 // app.use(cors());
 app.use(cors({
@@ -44,7 +69,7 @@ app.get('/problem', async (req, res) => {
   const query = 'SELECT * FROM problem WHERE id = ?';
   try {
     // 단일 문제
-    const [results] = await connection.query(query, [id]);
+    const [results] = await mysqlConn.query(query, [id]);
     if (results.length === 0) {
       return res.status(404).json({ error: '문제를 찾을 수 없습니다.' });
     }
@@ -59,7 +84,7 @@ app.get('/problem-set', async (req, res) => {
   try {
     // 문제 목록 가져오기
     const query = 'SELECT * FROM problem';
-    const [results] = await connection.query(query);
+    const [results] = await mysqlConn.query(query);
     res.json(results);
   } catch (err) {
     console.error('Database query error:', err);
@@ -72,7 +97,7 @@ app.get('/results', async (req, res) => {
   const query = 'SELECT * FROM code';
   try {
     // 문제 목록 가져오기
-    const [results] = await connection.query(query);
+    const [results] = await mysqlConn.query(query);
     res.json(results);
   } catch (err) {
     console.error('Database query error:', err);
@@ -89,7 +114,7 @@ app.get('/code', async (req, res) => {
   const query = 'SELECT * FROM code WHERE id = ?';
   try {
     // 문제 목록 가져오기
-    const [results] = await connection.query(query, [id]);
+    const [results] = await mysqlConn.query(query, [id]);
     if (results.length === 0) {
       return res.status(404).json({ error: '제출 결과를 찾을 수 없습니다.' });
     }
@@ -118,7 +143,7 @@ app.post('/submit', async (req, res) => {
     VALUES (?, ?, ?, 0)
   `;
   try {
-    await connection.query(query, [id, language, code]);
+    await mysqlConn.query(query, [id, language, code]);
     res.status(200).json({ message: 'Code submitted successfully!' });
   } catch (err) {
     console.error('Error inserting data into MySQL:', err);
