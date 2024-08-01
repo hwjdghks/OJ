eab6a32fb58a1189890be2b7bf5e1c499021d96f
Jeonghwan Hwang <jhwang@c2r10s1.42seoul.kr>
2024-08-01 21:34:31 +0900

    add link to / when click title

--- srcs/requirements/frontend/src/app/components/Navbar.js
+++ srcs/requirements/frontend/src/app/components/Navbar.js
@@ -4,21 +4,23 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.container}>
-                <h1>문제 목록</h1>
-                <ul className={styles.navList}>
-                    <li>
-                        <Link className={styles.navLink} href="/problem-set">문제 리스트</Link>
-                    </li>
-                    <li>
-                        <Link className={styles.navLink} href="/results">채점 결과</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <h1>
+          <Link className={styles.navLink} href="/">졸업 작품</Link>
+        </h1>
+        <ul className={styles.navList}>
+          <li>
+            <Link className={styles.navLink} href="/problem-set">문제 리스트</Link>
+          </li>
+          <li>
+            <Link className={styles.navLink} href="/results">채점 결과</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
