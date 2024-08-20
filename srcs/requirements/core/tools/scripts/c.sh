81f3706fc2e3fdafe084124c521bc14e4e35b6b8
Jeonghwan Hwang <jhwang@c1r4s1.42seoul.kr>
2024-08-20 22:21:50 +0900

    update grade script

--- srcs/requirements/core/tools/scripts/c.sh
+++ srcs/requirements/core/tools/scripts/c.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+gcc -o Main Main.c
+if [ $? -ne 0 ]; then
+    exit 40 # runtime error
+fi
 # 'in' 폴더 내부의 파일 개수 계산
 file_count=$(ls ./in | wc -l)
 
 # 파일 쌍 반복 비교
 for i in $(seq 1 $file_count); do
     # 프로그램 실행
-    user=$(./Main < ./in/$i.in 2>&1)
+    user=$(./Main < ./in/$i.in)
     if [ $? -ne 0 ]; then
         exit 40 # runtime error
     fi
