202bed5410ecb89c6e5c8b0e3cea0a2f45163a73
Jeonghwan Hwang <jhwang@c1r4s1.42seoul.kr>
2024-08-20 22:21:15 +0900

    add ack

--- srcs/requirements/backend/services/rabbitmqService.js
+++ srcs/requirements/backend/services/rabbitmqService.js
@@ -13,9 +13,11 @@ async function startConsume() {
   channel.consume(queue, async (msg) => {
     console.log("UPDATE REQUEST RECEIVED");
     const response = JSON.parse(msg.content.toString());
+    console.log(response);
     const { submit_id, result } = response;
     const sql = 'UPDATE code SET result = ? WHERE id = ?';
     await pool.query(sql, [result, submit_id]);
+    channel.ack(msg);
   });
 }
 
