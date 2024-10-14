3552f9c439d8f1211ad41169da8428fbc1176fcf
jhhwang <5832120@naver.com>
2024-10-15 01:27:45 +0900

    fix code

--- srcs/requirements/core/worker.py
+++ srcs/requirements/core/worker.py
@@ -3,10 +3,10 @@ from config import environ
 from grade import grade
 
 def worker(worker_id):
-    credentials = pika.PlainCredentials(environ['user'], environ['password'])
+    credentials = pika.PlainCredentials(environ['rabbitmq']['user'], environ['rabbitmq']['password'])
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
-            host=environ['host'],
+            host=environ['rabbitmq']['host'],
             credentials=credentials,
             connection_attempts=30,
             retry_delay=5
@@ -14,11 +14,11 @@ def worker(worker_id):
     )
     channel = connection.channel(worker_id)
 
-    channel.queue_declare(queue=environ['send_queue'])
-    channel.queue_declare(queue=environ['recv_queue'])
+    channel.queue_declare(queue=environ['rabbitmq']['send_queue'])
+    channel.queue_declare(queue=environ['rabbitmq']['recv_queue'])
 
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=environ['recv_queue'], on_message_callback=grade)
+    channel.basic_consume(queue=environ['rabbitmq']['recv_queue'], on_message_callback=grade)
 
     print('[*] Worker {0:d} waiting for messages. To exit press CTRL+C'.format(worker_id))
     channel.start_consuming()
