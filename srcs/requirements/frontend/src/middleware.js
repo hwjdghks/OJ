bde6537de8d2560fa8d4a5e3176018578fdc885b
jhhwang <5832120@naver.com>
2024-10-30 01:15:09 +0900

    fix code

--- srcs/requirements/frontend/src/middleware.js
+++ srcs/requirements/frontend/src/middleware.js
@@ -1,18 +1,16 @@
-export { withAuth } from "next-auth/middleware"
+// middleware.js
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export default withAuth(
-    // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    console.log(req.nextauth.token)
-  },
-  {
-    callbacks: {
-      authorized({ token }) {
-        if(token && token.is_admin === true) return true
-      }
-    },
+export async function middleware(req) {
+  const token = await getToken({ req });
+
+  if (!token || !token.is_admin) {
+    return NextResponse.rewrite(new URL("/404", req.url));
   }
-)
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/admin/:path*"],
