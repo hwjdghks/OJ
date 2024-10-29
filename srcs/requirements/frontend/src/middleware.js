7da45fa2329c7f8f54f31429f2a6166766808449
jhhwang <5832120@naver.com>
2024-10-30 01:40:04 +0900

    fix code

--- srcs/requirements/frontend/src/middleware.js
+++ srcs/requirements/frontend/src/middleware.js
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = await getToken({ req });
 
+  console.log('관리자 페이지 접근:', token);
   if (!token || !token.is_admin) {
     return NextResponse.rewrite(new URL("/404", req.url));
   }
