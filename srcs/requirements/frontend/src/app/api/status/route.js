2fa782c0eb8ac3cafc27b6cd2287c4015e11b843
jhhwang <5832120@naver.com>
2024-11-04 03:39:55 +0900

    fix code

--- srcs/requirements/frontend/src/app/api/status/route.js
+++ srcs/requirements/frontend/src/app/api/status/route.js
@@ -1,6 +1,6 @@
 // app/api/status/route.js
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/lib/auth';
+import { authOptions } from '../auth/[...nextauth]/route';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
