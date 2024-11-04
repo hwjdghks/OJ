00266decd26167c8c57550e9eeb2f1958fcdb4a9
jhhwang <5832120@naver.com>
2024-11-04 12:45:42 +0900

    fix code

--- srcs/requirements/backend/handlers/statusHandler.js
+++ srcs/requirements/backend/handlers/statusHandler.js
@@ -22,14 +22,14 @@ async function getStatusHandler(req, res) {
   try {
     const pool = await mysqlConnect();
     const [results] = await pool.query(query, [user_id]);
-
+    console.log('불러오기 결과:', results);
     // user_id가 암호화되어 있는 경우를 위한 복호화 처리
     const decryptedResults = results.map(result => ({
       code_id: result.code_id,
       language: result.language,
       title: result.title
     }));
-
+    console.log('변환 결과:', decryptedResults);
     res.status(200).json({
       results: decryptedResults
     });
